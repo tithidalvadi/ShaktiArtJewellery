8eff9c892f7a4cf67da128104e16cb3537a8309a
tithidalvadi <tithidalvadi@gmail.com>
2026-07-19 12:25:01 +0530

    Updated website

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function updateActiveNav() {
 backToTop.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
 /* ---------- COLLECTIONS DATA ---------- */
 const collections = [
-  { title: 'Bridal and occasional jewelry', image: 'images/collection1.png', link: 'design1.html' },
+  { title: 'Bridal and occasional jewellry', image: 'images/collection1.png', link: 'design1.html' },
   { title: 'Kundan jewellery raw material', image: 'images/collection2.png' , link: 'design2.html' },
   { title: 'Kundan rings & Kilangi', image: 'images/collection3.png' , link: 'design3.html' },
   { title: 'Kundan earrings & mangtika', image: 'images/collection5.png' , link: 'design5.html' },
